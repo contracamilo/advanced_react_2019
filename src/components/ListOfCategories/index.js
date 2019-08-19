bd0f9927fe3428a70545fbfd61beb63781c9efb3
camilo rivera <camilo.riveraq@gmail.com>
2019-08-19 02:19:39 -0500

    Addin useEffecc in the categories comp and setup for the unit test

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://camigram-server-k44zscah9.now.sh/categories')
@@ -13,17 +14,33 @@ export const ListOfCategories = () => {
       })
   }, []) // [] second param only one time
 
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {
+        categories.map((category, index) =>
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        )
+      }
+    </List>
+  )
+
   return (
     <React.Fragment>
-      <List>
-        {
-          categories.map((category, index) =>
-            <Item key={category.id}>
-              <Category {...category} />
-            </Item>
-          )
-        }
-      </List>
+      {renderList()}
+      {showFixed && renderList(true)}
     </React.Fragment>
   )
 }
---
+++ src/components/categories/Categories.test.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import { findByTestAttr } from '../../../utils/testUtils'
+import { Category } from './index'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const defaultProps = { success: false }
+
+/**
+ * Factory Function to create a ShallowWrapper for the Ccongrats component
+ * @function Setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<Category {...setupProps} />)
+}
+
+test('Rendering without errors', () => {
+  const wrapper = shallow(<Category />)
+  const component = findByTestAttr(wrapper, 'category')
+  expect(component.length).toBe(1)
+})
